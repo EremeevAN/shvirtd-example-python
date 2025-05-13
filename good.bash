b4872e485618de2e91bbec066415e71218be14a5
EremeevAN <lexa16042010@mail.ru>
2025-05-13 21:56:39 +0300

    Add files via upload

---
+++ good.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Проверка прав администратора
+if [ "$(id -u)" != "0" ]; then
+  echo "Этот скрипт должен выполняться от имени root"
+  exit 1
+fi
+apt update && \
+apt install -y git
+cd /opt
+if [ ! -d "/opt/shvirtd-example-python" ]; then
+  git clone https://github.com/NikitaLGit/shvirtd-example-python.git && \
+  cd shvirtd-example-python
+else
+  cd shvirtd-example-python
+fi
+docker compose up -d
+docker ps -a
+#curl http://localhost:8090
+exit 0
